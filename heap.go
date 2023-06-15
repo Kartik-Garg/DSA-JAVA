acc9628f790053f8f8ba256f86e60a47fd60ac8e
Kartik Garg <kartikgarg938@gmail.com>
2023-06-15 16:11:26 +0530

    Create heap.go
    not working :(

---
+++ heap.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/go-pg/pg/v10"
+	"github.com/go-pg/pg/v10/orm"
+)
+
+func main() {
+	// Connect to the database
+	db := pg.Connect(&pg.Options{
+		User:     "your_username",
+		Password: "your_password",
+		Database: "your_database",
+	})
+
+	// Close the database connection when done
+	
+	// Access the PRURL field
+	prURL := reviewRequest.PRURL
+	fmt.Println("PR URL:", prURL)
+}
